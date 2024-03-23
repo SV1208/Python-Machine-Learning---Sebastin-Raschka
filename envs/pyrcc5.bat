24916a2b323cc98e882ba0c3bfb75c4263fae8d7
RoboMaven <shivanshuv999@gmail.com>
2024-03-23 16:29:48 +0530

    added numpy=1.17.4 scipy=1.3.1 scikit-learn=0.22.0 matplotlib=3.1.0 pandas=0.25.3

---
+++ envs/pyrcc5.bat
@@ -0,0 +1,1 @@
+@C:/Users/Shivanshu/Desktop/WorkSpace/Python-Machine-Learning-Sebastian-Raschka/envs\python.exe -m PyQt5.pyrcc_main %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ envs/pyuic5.bat
@@ -0,0 +1,1 @@
+@C:/Users/Shivanshu/Desktop/WorkSpace/Python-Machine-Learning-Sebastian-Raschka/envs\python.exe -m PyQt5.uic.pyuic %1 %2 %3 %4 %5 %6 %7 %8 %9
